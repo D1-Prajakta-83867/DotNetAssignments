034e76a2a72b70346926f81cbc27439a15350b79
D1_83867_Prajakta <prajaktam06.comp@gmail.com>
2024-05-25 15:02:06 +0530

    Assignment 11

---
+++ Assignment 11/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        WageEmp wageEmp = new WageEmp();
+        wageEmp.Accept();
+        Console.WriteLine("_______________________________________________");
+        Console.WriteLine("\nWage Employee Details:");
+        wageEmp.Print();
+        Console.WriteLine("___________________________________________________________________________________________________");
+
+        Console.WriteLine("\nWage Employee Details (String Format):");
+        Console.WriteLine(wageEmp.ToString());
+        Console.WriteLine("___________________________________________________________________________________________________");
+
+        Console.ReadLine();
+    }
+}
---
+++ Assignment 11/WageEmp.cs
@@ -0,0 +1,51 @@
+﻿public class WageEmp : Employee
+{
+    private int hours;
+    private int rate;
+
+    public int Hours
+    {
+        get { return hours; }
+        set { hours = value; }
+    }
+
+    public int Rate
+    {
+        get { return rate; }
+        set { rate = value; }
+    }
+
+    public WageEmp() : base()
+    {
+        Designation = "Wage";
+    }
+
+    public WageEmp(string name, int age, double salary, DepartmentType dept, int hours, int rate) : base(name, age, salary, "Wage", dept)
+    {
+        Hours = hours;
+        Rate = rate;
+    }
+
+    public new void Accept()
+    {
+        base.Accept();
+
+        Console.Write("Enter Hours: ");
+        Hours = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Rate: ");
+        Rate = int.Parse(Console.ReadLine());
+    }
+
+    public new void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Hours: {Hours}");
+        Console.WriteLine($"Rate: {Rate}");
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Hours: {Hours}, Rate: {Rate}";
+    }
+}
