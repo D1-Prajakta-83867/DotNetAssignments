7e8973436eda84a32dbd7db5390617d97f1aaf23
D1_83867_Prajakta <prajaktam06.comp@gmail.com>
2024-05-25 14:59:52 +0530

    Assignment 9

---
+++ Assignment 9/Manager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Manager : Employee
+{
+    private double bonus;
+
+    public double Bonus
+    {
+        get { return bonus; }
+        set { bonus = value; }
+    }
+
+    public Manager() : base()
+    {
+        Designation = "Manager";
+    }
+
+    public Manager(string name, int age, double salary, DepartmentType dept, double bonus) : base(name, age, salary, "Manager", dept)
+    {
+        Bonus = bonus;
+    }
+
+    public new void Accept()
+    {
+        base.Accept();
+
+        Console.Write("Enter Bonus: ");
+        Bonus = double.Parse(Console.ReadLine());
+    }
+
+    public new void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Bonus: {Bonus}");
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Bonus: {Bonus}";
+    }
+}
+
---
+++ Assignment 9/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager manager = new Manager();
+        manager.Accept();
+        Console.WriteLine("\nManager Details:");
+        manager.Print();
+        Console.WriteLine("--------------------------------------------------------");
+
+       Console.WriteLine("\nManager Details (String Format):");
+        Console.WriteLine(manager.ToString());
+        Console.WriteLine("---------------------------------------------------------");
+
+        Console.ReadLine();
+    }
+}
