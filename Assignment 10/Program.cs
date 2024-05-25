92e8036214927b565abba958b387e33958654637
D1_83867_Prajakta <prajaktam06.comp@gmail.com>
2024-05-25 15:01:02 +0530

    Assignment 10

---
+++ Assignment 10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Supervisor supervisor = new Supervisor();
+        supervisor.Accept();
+        Console.WriteLine("____________________________________________________");
+
+        Console.WriteLine("\nSupervisor Details:");
+        supervisor.Print();
+        
+        Console.WriteLine("_______________________________________________________");
+        Console.WriteLine("\nSupervisor Details (String Format):");
+        Console.WriteLine(supervisor.ToString());
+        Console.WriteLine("________________________________________________________");
+
+        Console.ReadLine();
+    }
+}
---
+++ Assignment 10/Supervisor.cs
@@ -0,0 +1,40 @@
+﻿public class Supervisor : Employee
+{
+    private int subordinates;
+
+    public int Subordinates
+    {
+        get { return subordinates; }
+        set { subordinates = value; }
+    }
+
+    public Supervisor() : base()
+    {
+        Designation = "Supervisor";
+    }
+
+    public Supervisor(string name, int age, double salary, DepartmentType dept, int subordinates) : base(name, age, salary, "Supervisor", dept)
+    {
+        Subordinates = subordinates;
+    }
+
+    public new void Accept()
+    {
+        base.Accept();
+
+        Console.Write("Enter number of subordinates: ");
+        Subordinates = int.Parse(Console.ReadLine());
+    }
+
+    public new void Print()
+    {
+        base.Print();
+        Console.WriteLine($"Subordinates: {Subordinates}");
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", Subordinates: {Subordinates}";
+    }
+}
+
